3f3d516c7b9d1a0ec3c21b01aaf1c05ec99fde68
Hichamelhmaydi <ct1594065@gmail.com>
2024-12-11 23:05:28 +0100

    added sql file

---
+++ sql/gym.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE gym;
+USE gym ;
+CREATE TABLE activite (
+    ID_activite INT AUTO_INCREMENT PRIMARY KEY ,  
+    nom_activite VARCHAR(100) NOT NULL,                  
+    descr TEXT,                            
+    date_debut DATE NOT NULL,                   
+    date_fin DATE NOT NULL,                         
+    disponibilite TINYINT NOT NULL,                 
+    capacite INT  NOT NULL                                
+);
+
+
+CREATE TABLE membres (
+    ID_membres INT AUTO_INCREMENT PRIMARY KEY,  
+    nom VARCHAR(50) NOT NULL,                  
+    prenom VARCHAR(50) NOT NULL,                            
+    email VARCHAR(100) NOT NULL,                   
+    telephone VARCHAR(15) NOT NULL                                                      
+);
+
+
+CREATE TABLE reservations (
+    ID_reservations INT AUTO_INCREMENT PRIMARY KEY,
+    ID_activite INT NOT NULL,
+    ID_membres INT NOT NULL,
+    date_reservation DATETIME NOT NULL,
+    statu ENUM('Confirmé', 'Annulée') NOT NULL,
+    FOREIGN KEY (ID_activite) REFERENCES activite(ID_activite),
+    FOREIGN KEY (ID_membres) REFERENCES membres(ID_membres)
+);
