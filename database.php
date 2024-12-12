eecc2a6fa5e6d2309e31f32bda5ccf647cf4c605
Hichamelhmaydi <ct1594065@gmail.com>
2024-12-12 10:13:49 +0100

    added script php for connexion with my database

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+$db_service="localhost";
+$db_user="root";
+$db_password="";
+$db_name="gym";
+$conn="";
+$conn = mysqli_connect(
+        $db_service,
+        $db_user,
+        $db_password,
+        $db_name
+);
+?>
