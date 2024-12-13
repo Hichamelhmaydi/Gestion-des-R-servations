f1d17e9d79b17b5e3ee4c4f48087072cdc1926e6
Hichamelhmaydi <ct1594065@gmail.com>
2024-12-13 17:05:51 +0100

    last version 2

--- add_to_db.php
+++ add_to_db.php
@@ -10,27 +10,23 @@ $desc_act = $_POST["descr_act"];
 $date_debut = $_POST["date_debut"];
 $date_fin = $_POST["date_fin"];
 $disponibilite = $_POST["disponibilite"];
-$id_membre = mysqli_insert_id($conn);
-$id_activite = mysqli_insert_id($conn);
-$date_reservation = date("Y-m-d"); 
 
 $requete = "INSERT INTO membres(nom, prenom, email, telephone) VALUES('$nom', '$prenom', '$email', '$telephone')";
 $query = mysqli_query($conn, $requete);
 
-
+$id_membre = mysqli_insert_id($conn);
 
 $requete3 = "INSERT INTO activite (nom_activite, descr, date_debut, date_fin, disponibilite) VALUES('$nom_act', '$desc_act', '$date_debut', '$date_fin', '$disponibilite')";
 $query3 = mysqli_query($conn, $requete3);
 
+$id_activite = mysqli_insert_id($conn);
 
-
-
-$requete2 = "INSERT INTO reservations (ID_membres, ID_activite, date_reservation) VALUES ('$id_membre', '$id_activite', '$date_reservation')";
+$date_reservation = date("Y-m-d"); 
+$requete2 = "INSERT INTO reservations(ID_membres, ID_activite, date_reservation) VALUES ('$id_membre', '$id_activite', '$date_reservation')";
 $query2 = mysqli_query($conn, $requete2);
 
-
-
 ?>
+
 
 
 
--- aff_db.php
+++ aff_db.php
@@ -3,6 +3,7 @@ include 'database.php';
 
 $requete ="SELECT * from membres";
 $query = mysqli_query ($conn,$requete);
+echo"<h1>";echo"Membres";echo"</h1>";
 echo"<table border=1; width=800PX;>";
 echo "<th>ID</th>";
 echo "<th>nom</th>";
@@ -19,4 +20,28 @@ while($row=mysqli_fetch_array($query)){
     echo "</tr>";
 }
 echo"</table>";
+
+$requet ="SELECT * from activite";
+$quer = mysqli_query ($conn,$requet);
+echo"<h1>";echo"Activité";echo"</h1>";
+echo"<table border=1; width=800PX;>";
+echo "<th>ID</th>";
+echo "<th>Nom d'activite</th>";
+echo "<th>description</th>";
+echo "<th>date debut</th>";
+echo "<th>date fin</th>";
+echo "<th>disponibilite</th>";
+while($row=mysqli_fetch_array($quer)){
+    echo "<tr>";
+       echo "<td>".$row["ID_activite"]."</td>";
+       echo "<td>".$row["nom_activite"]."</td>";
+       echo "<td>".$row["descr"]."</td>";
+       echo "<td>".$row["date_debut"]."</td>";
+       echo "<td>".$row["date_fin"]."</td>";
+       echo "<td>".$row["disponibilite"]."</td>";
+    echo "</tr>";
+}
+echo"</table>";
+
+
 ?>
